080dfead0324d3e61305da40c480cebef63044fa
Ayush5ah <sahayush89@gmail.com>
2024-07-17 14:43:05 +0530

    second commit

---
+++ src/components/Modal.jsx
@@ -0,0 +1,24 @@
+import { createPortal } from 'react-dom';
+import {AnimatePresence, motion} from 'framer-motion';
+export default function Modal({ title, children, onClose }) {
+  return createPortal(
+    <>
+      <div className="backdrop" onClick={onClose} />
+      <AnimatePresence>
+      <motion.dialog 
+       variants={{
+        hidden: {opacity:0 ,y: 30},
+        visible: {opacity:1, y:0},
+       }}
+       initial="hidden"
+       animate="visible"
+       exit="hidden"
+      open className="modal">
+        <h2>{title}</h2>
+        {children}
+      </motion.dialog>
+      </AnimatePresence>
+    </>,
+    document.getElementById('modal')
+  );
+}
